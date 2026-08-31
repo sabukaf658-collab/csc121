b3d10fa3bd0abe3ca2e981aeb3fa9f4d57cdabf2
sabukaf658-collab <sabukaf@enoriveracademy.org>
2026-08-31 19:48:22 -0400

    module 0 labs

--- src/module_0/lab1_clock.py
+++ src/module_0/lab1_clock.py
@@ -1,8 +1,1 @@
 # Refer to this module's readme
-def test_should_print_updated_alarm_time(capsys):
-    assert hasattr(lab1_clock, "main"), "variables.py has no main() function. File may be blank"
-    lab1_clock.main()
-    captured = capsys.readouterr()
-    assert captured.out == "1741604700 in US/Eastern\n"
-
-    from module_0 import lab1_clock
