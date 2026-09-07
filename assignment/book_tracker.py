9467f6d4dbcf8770b7bb95e29c8e9ee9373cc90a
sabukaf658-collab <sabukaf@enoriveracademy.org>
2026-09-06 21:44:00 -0400

    add_book, dashboard, and estimated reading time functions

--- assignment/book_tracker.py
+++ assignment/book_tracker.py
@@ -4,13 +4,15 @@ def dashboard():
       📚  YOUR LIBRARY
     40 '='
     """
-    # your code here
+    print("=" * 40)
+    print("📚  YOUR LIBRARY")
+    print("=" * 40)
 
 
 def estimate_reading_time(pages):
     """Return estimated reading time in hours, assuming 40 pages/hour. 
     This number should be rounded to 1 decimal place"""
-    # your code here
+    return round(pages/ 40, 1)
 
 
 def add_book():
@@ -18,12 +20,11 @@ def add_book():
     This function takes in user input for title, author, and page count.
     Create a variable called hours that calls the function estimate_reading_time
     """
-    # your code here
-    title = # get user input in title case for "Book title: "
-    author = # get user input for "Author: "
-    pages = # get user input as an int for "Page count: " 
-    hours = # call estimate_reading_time by passing in pages
-    # use an f-string to print "'{title}' by {author} -- approx. {hours} to read"
+    title = input("Book title: ").title()
+    author = input("Author: ")
+    pages = int(input("Page count: "))
+    hours = estimate_reading_time(pages)
+    print(f"'{title}' by {author} -- approx. {hours} to read")
 
 
 def main():
